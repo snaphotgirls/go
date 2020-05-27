c60eacbecc6196e0a818e7140748ed64e067ee21
snaphotgirls <66030476+snaphotgirls@users.noreply.github.com>
2020-05-27 20:02:13 +0200

    Add files via upload

---
+++ js/trls.js
@@ -0,0 +1,307 @@
+var translation = {
+	source: {
+		title: "Looking for sex tonight in your area?",
+		text01: "Looking for sex tonight in your area?",
+		text02: "Are you older than 18 years?",
+		text03: "No",
+		text04: "Yes",
+		text05: "Many of these women are desperate single moms and cheating wives looking for some fun. They could be your neighbors or someone you know. Do you agree to keep the identity of these women a secret?",
+		text06: "Yes",
+		text07: "No",
+		text08: "These women have asked us to not allow men that are seeking a relationship. They only desire quick sex. Not dating. Do you agree to thisrequest?",
+		text09: "Yes",
+		text10: "No",
+		text11: "Do you agree to use a condom when having sex with a partner you meet on our site?",
+		text12: "Yes",
+		text13: "No",
+		text14: "Thank you. You may now see our list and photos of women who are in your area. Again, please keep their identity a secret.",
+		text15: "Continue"
+	},
+	en: {
+		title: "Looking for sex tonight in your area?",
+		text01: "Looking for sex tonight in your area?",
+		text02: "Are you older than 18 years?",
+		text03: "No",
+		text04: "Yes",
+		text05: "Many of these women are desperate single moms and cheating wives looking for some fun. They could be your neighbors or someone you know. Do you agree to keep the identity of these women a secret?",
+		text06: "Yes",
+		text07: "No",
+		text08: "These women have asked us to not allow men that are seeking a relationship. They only desire quick sex. Not dating. Do you agree to thisrequest?",
+		text09: "Yes",
+		text10: "No",
+		text11: "Do you agree to use a condom when having sex with a partner you meet on our site?",
+		text12: "Yes",
+		text13: "No",
+		text14: "Thank you. You may now see our list and photos of women who are in your area. Again, please keep their identity a secret.",
+		text15: "Continue"
+	},
+	de: {
+		title: "Das funktioniert besser als Tinder!",
+		text01: "Das funktioniert besser als Tinder!",
+		text02: "Was bist du?",
+		text03: "Mann",
+		text04: "Frau",
+		text05: "Was ist dein Geschlecht?",
+		text06: "Mann",
+		text07: "Frau",
+		text08: "Bist du alter als 18?",
+		text09: "Ja",
+		text10: "Nein",
+		text11: "Wurdest du ein Kondom verwenden wenn du Sex?",
+		text12: "Ja",
+		text13: "Nein",
+		text14: "Sie kÃ¶nnen sich jetzt unsere Liste sowie die Fotos von Frauen aus Ihrer Umgebung ansehen. BerÃ¼hren Sie fÃ¼r eine Suche gemÃ¤ÃŸ Ortskennnummer/Postleitzahl den nachfolgenden \"WEITER\" Button.",
+		text15: "Weiter"
+	},
+	da: {
+		title: "Bedre end Tinder!",
+		text01: "Bedre end Tinder!",
+		text02: "Leder du efter mÃ¦nd eller kvinder?",
+		text03: "MAND",
+		text04: "KVINDE",
+		text05: "Er du en mÃ¦nd eller kvinde?",
+		text06: "MAND",
+		text07: "KVINDE",
+		text08: "Er du mindst 18 Ã¥r gammel?",
+		text09: "JA",
+		text10: "NEJ",
+		text11: "Accepterer du at bruge kondom, nÃ¥r du har sex med en partner, som du mÃ¸der pÃ¥ vores site?",
+		text12: "JA",
+		text13: "NEJ",
+		text14: "Du kan nu se vores liste og fotos af kvinder i dit omrÃ¥de. Igen, hold venligst deres identitet hemmelig. Klik pÃ¥ knappen \"FortsÃ¦t\" og sÃ¸g inden for dit postnummer.",
+		text15: "FortsÃ¦t"
+	},
+	es: {
+		title: "Â¡Mejor que el Tinder!",
+		text01: "Â¡Mejor que el Tinder!",
+		text02: "Â¿EstÃ¡s buscando hombres o mujeres?",
+		text03: "MUJERES",
+		text04: "HOMBRE",
+		text05: "Â¿Eres un hombre o una mujer?",
+		text06: "MUJERES",
+		text07: "HOMBRE",
+		text08: "Por Ãºltimo, Â¿tienes al menos 18 aÃ±os?",
+		text09: "SÃ­",
+		text10: "No",
+		text11: "Â¿Aceptas usar condÃ³n cuando te acuestes con alguien que has conocido en nuestro sitio?",
+		text12: "SÃ­",
+		text13: "No",
+		text14: "Ahora puedes ver nuestra lista de mujeres en tu zona y tambiÃ©n puedes ver sus fotos. Haz clic en el botÃ³n â€œContinuarâ€ de abajo para efectuar una bÃºsqueda con tu cÃ³digo zip/postal.",
+		text15: "Continuar"
+	},
+	fi: {
+		title: "Parempi kuin Tinder!",
+		text01: "Parempi kuin Tinder!",
+		text02: "EtsitkÃ¶ miehiÃ¤ vai naisia?",
+		text03: "MIEHIÃ„",
+		text04: "NAISIA",
+		text05: "Oletko mies vai nainen?",
+		text06: "MIES",
+		text07: "NAINEN",
+		text08: "Oletko vÃ¤hintÃ¤Ã¤n 18-vuotias?",
+		text09: "KyllÃ¤",
+		text10: "Ei",
+		text11: "Suostutko kÃ¤yttÃ¤mÃ¤Ã¤n kondomia harrastaessasi seksiÃ¤ sivuston kautta tapaamasi partnerin kanssa?",
+		text12: "KyllÃ¤",
+		text13: "Ei",
+		text14: "Voit nyt tutustua alueesi naisten luetteloon ja kuviin. Kuten sanottu, pidÃ¤thÃ¤n heidÃ¤n henkilÃ¶llisyytensÃ¤ salaisuutena. Klikkaa â€Jatkaâ€-painiketta ja hae postinumerollasi.",
+		text15: "Jatka"
+	},
+	fr: {
+		title: "Mieux que Tinder!",
+		text01: "Mieux que Tinder!",
+		text02: "Avec qui veux-tu t'envoyer en l'air?",
+		text03: "UN HOMME",
+		text04: "UNE FEMME",
+		text05: "Tu es?",
+		text06: "UN HOMME",
+		text07: "UNE FEMME",
+		text08: "Et enfin, avez-vous 18 ans ou plus?",
+		text09: "QUI",
+		text10: "NON",
+		text11: "Accepteriez-vous d'utiliser un prÃ©servatif lors des rapports sexuels avec un partenaire que vous rencontrez sur notre site?",
+		text12: "QUI",
+		text13: "NON",
+		text14: "Vous pouvez maintenant accÃ©der Ã  la liste et aux photos des femmes qui sont dans votre rÃ©gion. Tapez sur le bouton Â« Continuer Â» ci-dessous pour lancer votre recherche Ã  l'aide de votre code postal.",
+		text15: "Continuer"
+	},
+	it: {
+		title: "Meglio di Tinder!",
+		text01: "Meglio di Tinder!",
+		text02: "Cerchi uomini o donne?",
+		text03: "UOMINI",
+		text04: "DONNE",
+		text05: "Sei uomo o donna?",
+		text06: "UOMO",
+		text07: "DONNA",
+		text08: "Hai almeno 18 anni?",
+		text09: "SÃ¬",
+		text10: "No",
+		text11: "Sei dâ€™accordo allâ€™utilizzo del preservativo ogni volta che fai sesso con una partner che hai conosciuto nel nostro sito?",
+		text12: "SÃ¬",
+		text13: "No",
+		text14: "Ora puoi vedere la lista e le foto delle donne disponibili nella tua zona. Clicca su \"Continua\" per fare una ricerca con il tuo codice postale.",
+		text15: "Continua"
+	},
+	nl: {
+		title: "Beter dan Tinder!",
+		text01: "Beter dan Tinder!",
+		text02: "Ben je op zoek naar mannen of vrouwen?",
+		text03: "MANNEN",
+		text04: "VROUWEN",
+		text05: "Ben je een man of een vrouw?",
+		text06: "MAN",
+		text07: "VROUW",
+		text08: "Tot slot, ben je minstens 18 jaar oud?",
+		text09: "JA",
+		text10: "NEE",
+		text11: "Ben je het ermee eens om een â€‹â€‹condoom te gebruiken bij seks met een partner die je ontmoet op onze site?",
+		text12: "JA",
+		text13: "NEE",
+		text14: "Je kunt nu onze lijst en foto's van vrouwen die in jouw omgeving wonen zien. Tik op de \"Doorgaan\"-knop hieronder om te zoeken binnen je postcodegebied.",
+		text15: "Doorgaan"
+	},
+	no: {
+		title: "Bedre enn Tinder!",
+		text01: "Bedre enn Tinder!",
+		text02: "Er du ute etter menn eller kvinner?",
+		text03: "MENN",
+		text04: "KVINNER",
+		text05: "Er du mann eller kvinne?",
+		text06: "MENN",
+		text07: "KVINNER",
+		text08: "Er du minst 18 Ã¥r gammel?",
+		text09: "JA",
+		text10: "NEI",
+		text11: "Samtykker du Ã¥ bruke kondom nÃ¥r du har sex med en partner du har mÃ¸tt via nettstedet vÃ¥rt?",
+		text12: "JA",
+		text13: "NEI",
+		text14: "Du kan nÃ¥ se listen vÃ¥r over kvinner som er i ditt omrÃ¥de, og du kan se bildene deres. Trykk pÃ¥ \"Fortsett\"-knappen under for Ã¥ sÃ¸ke med ditt postnummer.",
+		text15: "Fortsett"
+	},
+	pt: {
+		title: "Melhor que o Tinder!",
+		text01: "Melhor que o Tinder!",
+		text02: "VocÃª estÃ¡ procurando por um homem ou mulher?",
+		text03: "HOMEM",
+		text04: "MULHER",
+		text05: "VocÃª Ã© um homem ou uma mulher?",
+		text06: "HOMEM",
+		text07: "MULHER",
+		text08: "VocÃª tem pelo menos 18 anos de idade?",
+		text09: "Sim",
+		text10: "NÃ£o",
+		text11: "VocÃª concorda em usar camisinha ao transar com alguÃ©m que vocÃª conheÃ§a atravÃ©s do nosso site?",
+		text12: "Sim",
+		text13: "NÃ£o",
+		text14: "Agora vocÃª jÃ¡ pode conferir a nossa lista e fotos de mulheres aÃ­ da sua regiÃ£o. Clique no botÃ£o \"Continuar\" e pesquise de acordo com o seu CEP.",
+		text15: "Continuar"
+	},
+	ru: {
+		title: "Ð¥Ð¾Ñ‡ÐµÑˆÑŒ Ð·Ð°Ð½ÑÑ‚ÑŒÑÑ ÑÐµÐºÑÐ¾Ð¼ ÑÐµÐ³Ð¾Ð´Ð½Ñ Ð²ÐµÑ‡ÐµÑ€Ð¾Ð¼?",
+		text01: "Ð¥Ð¾Ñ‡ÐµÑˆÑŒ Ð·Ð°Ð½ÑÑ‚ÑŒÑÑ ÑÐµÐºÑÐ¾Ð¼ ÑÐµÐ³Ð¾Ð´Ð½Ñ Ð²ÐµÑ‡ÐµÑ€Ð¾Ð¼?",
+		text02: "Ð’Ð°Ð¼ ÑƒÐ¶Ðµ ÐµÑÑ‚ÑŒ 18?",
+		text03: "ÐÐµÑ‚",
+		text04: "Ð”Ð°",
+		text05: "ÐœÐ½Ð¾Ð³Ð¸Ðµ Ð¸Ð· ÑÑ‚Ð¸Ñ… Ð¶ÐµÐ½Ñ‰Ð¸Ð½ â€“ Ð¾Ñ‚Ñ‡Ð°ÑÐ²ÑˆÐ¸ÐµÑÑ Ð¼Ð°Ñ‚ÐµÑ€Ð¸-Ð¾Ð´Ð¸Ð½Ð¾Ñ‡ÐºÐ¸ Ð¸ Ð·Ð°Ð¼ÑƒÐ¶Ð½Ð¸Ðµ Ð´Ð°Ð¼Ñ‹, Ð¸Ñ‰ÑƒÑ‰Ð¸Ðµ Ð¿Ñ€Ð¸ÐºÐ»ÑŽÑ‡ÐµÐ½Ð¸Ð¹ Ð½Ð° ÑÑ‚Ð¾Ñ€Ð¾Ð½Ðµ. ÐžÐ½Ð¸ Ð¼Ð¾Ð³ÑƒÑ‚ Ð¾ÐºÐ°Ð·Ð°Ñ‚ÑŒÑÑ Ð²Ð°ÑˆÐ¸Ð¼Ð¸ ÑÐ¾ÑÐµÐ´ÐºÐ°Ð¼Ð¸ Ð¸Ð»Ð¸ Ð·Ð½Ð°ÐºÐ¾Ð¼Ñ‹Ð¼Ð¸. Ð’Ñ‹ ÑÐ¾Ð³Ð»Ð°ÑÐ½Ñ‹ ÑÐ¾Ñ…Ñ€Ð°Ð½Ð¸Ñ‚ÑŒ Ð¸Ð½Ñ„Ð¾Ñ€Ð¼Ð°Ñ†Ð¸ÑŽ Ð¾ Ð½Ð¸Ñ… Ð² ÑÐµÐºÑ€ÐµÑ‚Ðµ?",
+		text06: "Ð”Ð°",
+		text07: "ÐÐµÑ‚",
+		text08: "Ð­Ñ‚Ð¸ Ð¶ÐµÐ½Ñ‰Ð¸Ð½Ñ‹ Ð¿Ð¾Ð¿Ñ€Ð¾ÑÐ¸Ð»Ð¸ Ð½Ð°Ñ Ð½Ðµ Ð¿Ñ€ÐµÐ´Ð¾ÑÑ‚Ð°Ð²Ð»ÑÑ‚ÑŒ Ð¸Ð½Ñ„Ð¾Ñ€Ð¼Ð°Ñ†Ð¸ÑŽ Ð¾ Ð½Ð¸Ñ… Ð¼ÑƒÐ¶Ñ‡Ð¸Ð½Ð°Ð¼, Ñ€Ð°ÑÑÑ‡Ð¸Ñ‚Ñ‹Ð²Ð°ÑŽÑ‰Ð¸Ð¼ Ð½Ð° Ð¾Ñ‚Ð½Ð¾ÑˆÐµÐ½Ð¸Ñ. Ð‘Ñ‹ÑÑ‚Ñ€Ñ‹Ð¹ ÑÐµÐºÑ - ÑÑ‚Ð¾ Ð²ÑÐµ, Ñ‡ÐµÐ³Ð¾ Ð¾Ð½Ð¸ Ð¶Ð°Ð¶Ð´ÑƒÑ‚. Ð’Ñ‹ ÑÐ¾Ð³Ð»Ð°ÑÐ½Ñ‹?",
+		text09: "Ð”Ð°",
+		text10: "ÐÐµÑ‚",
+		text11: "Ð’Ñ‹ ÑÐ¾Ð³Ð»Ð°ÑÐ½Ñ‹ Ð¿Ð¾Ð»ÑŒÐ·Ð¾Ð²Ð°Ñ‚ÑŒÑÑ Ð¿Ñ€ÐµÐ·ÐµÑ€Ð²Ð°Ñ‚Ð¸Ð²Ð¾Ð¼ Ð²Ð¾ Ð²Ñ€ÐµÐ¼Ñ ÑÐµÐºÑÐ°?",
+		text12: "Ð”Ð°",
+		text13: "ÐÐµÑ‚",
+		text14: "ÐŸÐ¾Ð·Ð´Ñ€Ð°Ð²Ð»ÑÐµÐ¼! Ð’Ð°Ð¼ Ñ€Ð°Ð·Ñ€ÐµÑˆÐµÐ½ Ð´Ð¾ÑÑ‚ÑƒÐ¿ Ðº Ð½Ð°ÑˆÐµÐ¼Ñƒ ÑÐ°Ð¹Ñ‚Ñƒ. Ð¢ÐµÐ¿ÐµÑ€ÑŒ Ð¼Ð¾Ð¶Ð½Ð¾ Ð·Ð°Ñ€ÐµÐ³Ð¸ÑÑ‚Ñ€Ð¸Ñ€Ð¾Ð²Ð°Ñ‚ÑŒÑÑ Ð¸ Ð¿Ñ€Ð¸ÑÑ‚ÑƒÐ¿Ð¸Ñ‚ÑŒ Ðº Ð¿Ð¾Ð¸ÑÐºÑƒ ÑÐµÐºÑÑƒÐ°Ð»ÑŒÐ½Ð¾Ð³Ð¾ Ð¿Ð°Ñ€Ñ‚Ð½Ñ‘Ñ€Ð°.",
+		text15: "ÐŸÑ€Ð¾Ð´Ð¾Ð»Ð¶Ð¸Ñ‚ÑŒ"
+	},
+	sv: {
+		title: "BÃ¤ttre Ã¤n Tinder!",
+		text01: "BÃ¤ttre Ã¤n Tinder!",
+		text02: "SÃ¶ker du efter mÃ¤n eller kvinnor?",
+		text03: "MÃ„N",
+		text04: "KVINNOR",
+		text05: "Ã„r du man eller kvinna?",
+		text06: "MAN",
+		text07: "KVINNA",
+		text08: "Slutligen, Ã¤r du minst 18 Ã¥r gammal?",
+		text09: "JA",
+		text10: "NEJ",
+		text11: "Samtycker du till att anvÃ¤nda kondom nÃ¤r du har sex med en partner du trÃ¤ffat pÃ¥ vÃ¥r sajt?",
+		text12: "JA",
+		text13: "NEJ",
+		text14: "Du fÃ¥r nu lov att se vÃ¥r lista med kvinnor som befinner sig i ditt omrÃ¥de. Du fÃ¥r Ã¤ven se deras foton. Tryck pÃ¥ \"FortsÃ¤tt\"-knappen nedan fÃ¶r att sÃ¶ka med ditt postnummer.",
+		text15: "FortsÃ¤tt"
+	},
+	cs: {
+		title: "To funguje lÃ©pe neÅ¾ Tinder",
+		text01: "To funguje lÃ©pe neÅ¾ Tinder",
+		text02: "Kdo jsi?",
+		text03: "MuÅ¾",
+		text04: "Å½ena",
+		text05: "A koneÄnÄ›, je vÃ¡m alespoÅˆ 25 let?",
+		text06: "Ano",
+		text07: "Ne",
+		text08: "SouhlasÃ­te s tÃ­m, Å¾e zachovÃ¡te utajenou identitu tÄ›chto koÄiÄek?",
+		text09: "Ano",
+		text10: "Ne",
+		text11: "Potvrzujete, Å¾e pouÅ¾ijete pÅ™i sexu s partnerem, kterÃ©ho jste potkali na naÅ¡ich strÃ¡nkÃ¡ch, kondom?",
+		text12: "Ano",
+		text13: "Ne",
+		text14: "NynÃ­ se mÅ¯Å¾ete podÃ­vat na fotky Å¾en Å¾ijÃ­cÃ­ ve vaÅ¡em okolÃ­. JeÅ¡tÄ› jednou vÃ¡s Å¾Ã¡dÃ¡me, abyste uchovali jejich totoÅ¾nost v utajenÃ­. KliknÄ›te na tlaÄÃ­tko PokraÄovat a vyhledÃ¡vejte na zÃ¡kladÄ› vaÅ¡ich odpovÄ›dÃ­.",
+		text15: "PokraÄovat"
+	},
+};
+
+
+function replace_text(lang, text) {
+	x = document.getElementById(text); //Find the element with id="demo"
+	if (x) { //if element exisits
+		x.innerHTML = translation[lang][text];   //Change the HTML content
+	} else {
+		console.log("element not Found: " + text);
+	}
+}
+
+function translation_available(lang) {
+	if (translation[lang]) {
+		return lang;
+	} else {
+		console.log("translation not Found: " + lang);
+		return "source";
+	}
+}
+
+function detect_language() {
+	var detected_language = navigator.languages && navigator.languages[0] || navigator.language || navigator.userLanguage;
+	if (detected_language == "zh-CN") {
+		detected_language = "zh-Hans";
+	} else if (detected_language == "zh-SG") {
+		detected_language = "zh-Hans";
+	} else if (detected_language == "zh-MY") {
+		detected_language = "zh-Hans";
+	} else if (detected_language == "zh-CHS") {
+		detected_language = "zh-Hans";
+	} else if (detected_language == "zh-HK") {
+		detected_language = "zh-Hant";
+	} else if (detected_language == "zh-MO") {
+		detected_language = "zh-Hant";
+	} else if (detected_language == "zh-TW") {
+		detected_language = "zh-Hant";
+	} else if (detected_language == "zh-CHT") {
+		detected_language = "zh-Hant";
+	} else if (detected_language.length > 2) {
+		detected_language = detected_language[0] + detected_language[1];
+	}
+	;
+	return translation_available(detected_language);
+}
+
+function translate() {
+	var detected_language = detect_language();
+
+	for (x in translation["source"]) {
+		replace_text(detected_language, x);
+	}
+
+}
